5bda696d98ec4327e1eae6f5b160f43db614b003
Naga Venkata Praveen Vetsa <137479055+Praveenvetsa@users.noreply.github.com>
2023-09-03 20:12:39 +0530

    Add files via upload

---
+++ DecisionTree Regression.py
@@ -0,0 +1,35 @@
+# Decision Tree Regression
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(r'C:\Users\LENOVO\OneDrive\Desktop\fsds materials\fsds\3. Aug\22nd\EMP SAL.csv')
+X = dataset.iloc[:,1:2].values
+y = dataset.iloc[:,2].values
+
+from sklearn.tree import DecisionTreeRegressor
+#regressor = DecisionTreeRegressor()
+regressor = DecisionTreeRegressor(criterion='poisson',splitter='best',random_state =0)
+regressor.fit(X,y)
+
+y_pred = regressor.predict([[6.5]])
+y_pred
+
+%matplotlib inline
+plt.scatter(X,y,color = 'red')
+plt.plot(X,regressor.predict(X),color ='blue')
+plt.title('Truth or Bluff (Decision tree regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+X_grid = np.arange(min(X),max(X),0.01)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y,color = 'red')
+plt.plot(X,regressor.predict(X),color ='blue')
+plt.title('Truth or Bluff (Decision tree regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
